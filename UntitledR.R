0d54c1fd41a11ca57aefc801763601dcafef873d
angelaachen <144882700+angelaachen@users.noreply.github.com>
2023-09-27 14:57:03 +0800

    陳昱安
    411273067

---
+++ UntitledR.R
@@ -0,0 +1,56 @@
+c("John", "Mary")
+c("John", "Mary")
+
+list("John", 178, TRUE)
+personName= c("John", "Mary")
+interestingNumber= c(2,3.1412)
+covidPositive= c(TRUE, TRUE, F)
+
+typeof(c("a","b"))
+typeof(c("c","d"))
+
+
+list("John",178,TRUE)
+listExample = list("John",178,TRUE)
+
+
+recordExample1=list("John",37,178,FALSE)
+recordExample2=list(
+  "name"="John",
+  "age"=38,
+  "height"=178,
+  "isMarried"=F
+)
+
+recordExample1[[1]]
+recordExample2[[1]]
+recordExample2$name
+recordExample2[["name"]]
+recordExample2$'is married'
+
+listExample
+listExample[c(1,3)]
+
+atomicExample <- c(1,4,78,455,-23)
+atomicExample
+atomicExample[c(1,3,4)]
+atomicExample[c(1,2,3,4)]
+atomicExample[c(1:4)]
+atomicExample[c(2:5)]
+
+recordExample2
+recordExample2[c("name","height","is married")]
+
+persona1=list(
+  "name"="John",
+  "age"=38,
+  "height"=178,
+  "is married"=F
+)
+person2=list(
+  "name"="Mary",
+  "age"=32,
+  "height"=168,
+  "is married"=T)
+collective_data=list
+
