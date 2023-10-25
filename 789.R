8941c72c787c461df15e7d5d22aec4d0f202bf3b
angelaachen <144882700+angelaachen@users.noreply.github.com>
2023-10-25 15:02:08 +0800

    Add files via upload

---
+++ 789.R
@@ -0,0 +1,66 @@
+
+jsonlite::fromJSON("dats/international_flights.json")
+
+-
+  
+  
+  
+  data1 <- list(
+    file = "data/international_flights.json",
+    meta = list(
+      name = "國際航空定期時刻表",
+      source_link = "https://data.gov.tw/dataset/161167")
+  )
+  
+  
+  
+  flights <- list(
+    data = list(data1)
+  )
+  
+  saveRDS(flights, "data/flights.rds")
+  
+  filepath = flights$data[[1]]$file
+  flightsData <- jsonlite::fromJSON(filepath)
+  
+  flights$data[[1]]$data_frame <- flightsData
+  
+  saveRDS(flights, "data/flights.rds")
+  
+  typeof(flightsData)
+  class(flightsData)
+
+
+flights = readRDS("data/flights.rds")
+str(flights)
+
+flightsData <- flights$data[[1]]$data_frame
+
+
+flightsData$AirlineID
+
+
+
+
+levels(flightsData$AirlineID) |> length()
+table(flightsData$AirlineID) |> sort(decreasing = TRUE) |> head(10)
+data2 <- list(
+  meta = list(
+    name="航空公司統一代碼
+",
+    source_link ="https://data.gov.tw/dataset/8088"
+  ),
+  file = "data/airlines.json"
+)
+
+flights$data[[2]] <- data2
+
+airlines <-
+  jsonlite::fromJSON(
+    flights$data[[2]]
+    data[[2]]$data_frame <- airlines
+    
+    saveRDS(flights, file="flights.rds")
+    
+    
+    dplyr::glimpse(airlines)  
