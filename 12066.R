3b053315ef38ba5136d14fcb2b9af466658e0ccb
angelaachen <144882700+angelaachen@users.noreply.github.com>
2023-12-06 13:39:21 +0800

    Add files via upload

---
+++ 12066.R
@@ -0,0 +1,15 @@
+oneComingStudentMajor <- sample(c("Economics", "Sociology"), 1)
+
+# oneComingStudentMajor == "Economics"
+print("Please go to room 201.")
+
+# oneComingStudentMajor == "Sociology"
+print("Please go to room 301.")
+
+if(oneComingStudentMajor == "Economics"){
+  print("Please go to room 201.")
+}
+
+if(oneComingStudentMajor == "Sociology"){
+  print("Please go to room 301.")
+}
